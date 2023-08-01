8b4e1f767146579d64807d5b750b82d515fa1b13
triplee12 <tripleeoliver@gmail.com>
2023-08-01 22:14:35 +0100

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -15,38 +15,38 @@ if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
 
-n: int = int(sys.argv[1])
+n = int(sys.argv[1])
 
 
-def n_queens(n: int, i: int = 0, a: list = [], b: list = [], c: list = []):
+def n_queens(m, i=0, a=[], b=[], c=[]):
     """
     Find possible positions.
 
     Args:
-        n (int): number of queens
+        m (int): number of queens
         i (int): interation
         a (list): list of numbers
         b (list): rows
         c (list): columns
     """
-    if i < n:
-        for j in range(n):
+    if i < m:
+        for j in range(m):
             if j not in a and i + j not in b and i - j not in c:
-                yield from n_queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+                yield from n_queens(m, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
 
 
-def check(n: int):
+def check(m):
     """
     Check.
 
     Args:
-        n (int): number of queens
+        m (int): number of queens
     """
-    k: list = []
+    k = []
     i = 0
-    for solution in n_queens(n, 0):
+    for solution in n_queens(m, 0):
         for s in solution:
             k.append([i, s])
             i += 1
