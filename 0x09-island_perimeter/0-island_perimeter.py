93f4198ab0863fa7ac732fc29c3279aede3b8794
triplee12 <tripleeoliver@gmail.com>
2023-08-31 23:49:30 +0100

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,25 +9,22 @@ def island_perimeter(grid: List) -> int:
     Island perimeter.
 
     Args:
-        grid (list): list of cells
+        grid (list): 2D list containing 0(water) or 1(land)
     Returns:
-        Returns int, the number of the perimeter.
+        Returns int, the number of the perimeter of the island.
     """
-    if not grid:
-        return 0
-
     perimeter: int = 0
-    rows, cols = len(grid), len(grid[0])
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4
-
-                # Check adjacent cells
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    perimeter += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    perimeter += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    perimeter += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    perimeter += 1
 
     return perimeter
